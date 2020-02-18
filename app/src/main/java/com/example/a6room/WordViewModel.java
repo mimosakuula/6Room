9ec4eeebcc91287b50f3e05d88057dca455e693d
mimosakuula <45483110+mimosakuula@users.noreply.github.com>
2020-02-18 19:17:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a6room/WordViewModel.java
@@ -0,0 +1,27 @@
+package com.example.a6room;
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    public WordRepository mRepository;
+    public LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
